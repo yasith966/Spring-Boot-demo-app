6483b935532d47ab30e17fcde94d8fd16b2beb17
yasith996 <yasithnawanjana996@gmail.com>
2021-08-25 01:39:07 +0530

    fourth commit

---
+++ src/main/java/com/sliit_project/sliitproject/SliitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sliit_project.sliitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SliitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sliit_project/sliitproject/SliitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit_project.sliitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
